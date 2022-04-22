dee8daca907ef1eb7803b23c46871b226c01c689
Elanur Basaran <elanurbasaran7@gmail.com>
2022-04-22 09:47:59 +0300

    first

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const fs = require('fs')
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("text_text",(req,res)=>{
+app.get('test_html',(req,res)=>{
+    res.send("<h1>Elanur Başaran</h1>")
+})
 
+app.get('test_json',(req,res)=>{
+    res.send(
+        {
+            ad:"Elanur",
+        },
+        {
+            soyad:"Başaran"
+        },
+        {
+            numara:19360859020
+        }
+    )
 })
 
 app.listen(port,()=>{
