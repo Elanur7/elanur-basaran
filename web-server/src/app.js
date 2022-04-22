069c9d51b6c7c26c9b0aa505b91879f8b5c7c663
Elanur Basaran <elanurbasaran7@gmail.com>
2022-04-22 10:25:59 +0300

    first

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs')
 
 const app = express();
-const directory = path.join("../src");
+const directory = path.join("../web-server/src");
 app.use(express.static(directory));
 
 const port = process.env.PORT || 3000;
